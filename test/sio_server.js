c0ea9160c4f31feda3fbcd8ee32dc3539a44c05b
BubuHub <66834304+BubuHub@users.noreply.github.com>
2023-10-27 12:55:18 +0200

    Test code added (nodejs)

---
+++ test/sio_server.js
@@ -0,0 +1,33 @@
+const
+    {Server} = require("socket.io"),
+    server = new Server(8010, {
+	allowEIO3: true // false by default
+    });
+
+let
+    sequenceNumberByClient = new Map();
+
+// event fired every time a new client connects:
+server.on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    // initialize this client's sequence number
+    sequenceNumberByClient.set(socket, 1);
+
+    socket.on("ble", (v) => {
+        console.info(`ble = ${v}`);
+    });
+
+    // when socket disconnects, remove it from the list:
+    socket.on("disconnect", () => {
+        sequenceNumberByClient.delete(socket);
+        console.info(`Client gone [id=${socket.id}]`);
+    });
+});
+
+// sends each client its current sequence number
+setInterval(() => {
+    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+        client.emit("seq-num", sequenceNumber);
+        sequenceNumberByClient.set(client, sequenceNumber + 1);
+    }
+}, 1000);
